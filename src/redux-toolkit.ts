1e2a474225d971a304a7c1fa940ceb0a747965b4
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2021-11-28 20:13:23 -0600

    [ @Reduxjs/toolkit ] ~ [ extraReducers ] - implement counter slice

--- src/redux-toolkit.ts
+++ src/redux-toolkit.ts
@@ -60,3 +60,15 @@ const selectedTodoSlice = createSlice({
     select: (state, { payload }: PayloadAction<{ id: string }>) => payload.id,
   },
 });
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {},
+  extraReducers: {
+    [todosSlice.actions.create.type]: (state) => state + 1,
+    [todosSlice.actions.edit.type]: (state) => state + 1,
+    [todosSlice.actions.remove.type]: (state) => state + 1,
+    [todosSlice.actions.toggle.type]: (state) => state + 1,
+  },
+});
